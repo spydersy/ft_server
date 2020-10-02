f55f76fb42c1f2fa4dee5dd51d39b752fd00f136
abelarif <acbe1325@aol.com>
2020-10-02 05:59:20 -0400

    add php installation file

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN			apt-get update				-y
 RUN			apt-get upgrade				-y
 RUN			apt-get install nginx		-y
 RUN			apt-get install wget unzip	-y
-COPY		srcs/ /srcs
-RUN			./srcs/ysql_config.sh
+# COPY		srcs/ /srcs
+# RUN			./srcs/ysql_config.sh
 # RUN			./srcs/php_config.sh
 # RUN			./srcs/wp_config.sh
--- srcs/mysql_config.sh
+++ srcs/mysql_config.sh
@@ -2,7 +2,7 @@
 wget	https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 
 # Change permission :
-chmod	+x wget mysql-apt-config_0.8.15-1_all.deb
+chmod	+x mysql-apt-config_0.8.15-1_all.deb
 
 # Insert new values into the debconf database :
 echo	'mysql-apt-config mysql-apt-config/select-server select mysql-5.7' | debconf-set-selections
@@ -23,14 +23,14 @@ chown -R mysql: /var/lib/mysql
 service mysql start
 
 # Create User named by 'abelarif and identified by 'abelarif' & restrict access to localhost :
-mysql -u root --exectue "CREATE USER 'abelarif'@'localhost' IDENTIFIED BY 'abelarif';"
+mysql -u root -e "CREATE USER 'abelarif'@'localhost' IDENTIFIED BY 'abelarif';"
 
 # Create DataBase named by wp_DB :
-mysql -u root --exectue "CREATE DATABASE wp_DB;"
+mysql -u root -e "CREATE DATABASE wp_DB;"
 
 # Grant all access to the user :
-mysql -u root --exectue "GRANT ALL PRIVILEGES ON wp_DB.* TO 'abelarif'@'localhost';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wp_DB.* TO 'abelarif'@'localhost';"
 
 # Reloads the grant tables in the mysql database enabling the changes to take effect \
 # without reloading or restarting mysql service
-mysql -u root --exectue "FLUSH PRIVILEGES;"
+mysql -u root -e "FLUSH PRIVILEGES;"
